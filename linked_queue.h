36f75ba5320895a7caf0a6e15d238f004671d663
guan <guan.wenj@qq.com>
2025-08-21 11:27:27 +0800

    完成了模板的学习，将栈和队列模板化

--- linked_queue.h
+++ linked_queue.h
@@ -1,5 +1,5 @@
 //
-// Created by guan on 2025/8/19.
+// Created by guan on 2025/8/21.
 //
 
 #ifndef C__PRIME_LINKED_QUEUE_H
@@ -8,14 +8,14 @@
 
 
 
-
+template <typename T>
 class LinkedQueue {
     //结点 结构
     // val next  val 先用int  后续用模板 泛型
     struct Node {
-        int val;
+        T val;
         Node* next{nullptr};
-        explicit Node(int value):val(value){}
+        explicit Node(T value):val(value){}
     };
     Node *front;
     Node *rear;
@@ -29,9 +29,9 @@ class LinkedQueue {
     LinkedQueue& operator=(const LinkedQueue&) = delete;
 
     //push pop peek
-    void push(int value);
-    int pop();
-    int peek() const;
+    void push(T value);
+    T pop();
+    T peek() const;
     //get size   display
     bool is_empty() const;
     int get_size() const;
@@ -39,6 +39,93 @@ class LinkedQueue {
 };
 
 
+#include <print>
+#include <stdexcept>
+template <typename T>
+LinkedQueue<T>::LinkedQueue() {
+    size=0;
+    front=rear=nullptr;
+}
+
+
+template <typename T>
+LinkedQueue<T>::~LinkedQueue() {
+    size=0;
+    auto curr=front;
+    while (front!=rear) {
+        front=front->next;
+        delete curr;
+        curr=front;
+    }
+    delete front;
+    front = rear = nullptr;
+}
+
+template <typename T>
+void LinkedQueue<T>::push(T value) {
+    auto new_node=new Node(value);
+    if (this->is_empty()) {
+        front=rear=new_node;
+    }else {
+        rear->next=new_node;
+        rear=new_node;
+    }
+    ++size;
+}
+
+
+template <typename T>
+T LinkedQueue<T>::pop() {
+    if (this->is_empty()) {
+        throw std::runtime_error("Queue is Empty");
+    }
+    T value=front->val;
+    auto curr=front;
+    front=front->next;
+    delete curr;
+    --size;
+    if (this->is_empty()) {
+        front=rear=nullptr;
+    }
+    return value;
+}
+
+template <typename T>
+T LinkedQueue<T>::peek() const{
+    if (this->is_empty()) {
+        throw std::runtime_error("Queue is Empty");
+    }
+    return front->val;
+}
+
+
+template <typename T>
+
+bool LinkedQueue<T>::is_empty() const {
+    return size==0;
+}
+
+
+template <typename T>
+
+int LinkedQueue<T>::get_size() const{
+    return size;
+}
+
+
+template <typename T>
+void LinkedQueue<T>::display() const{
+    if (this->is_empty()) {
+        std::println("Queue is Empty");
+        return;
+    }
+    auto curr=front;
+    while (curr!=rear) {
+        std::print("{}->", curr->val);
+        curr=curr->next;
+    }
+    std::println("{}", curr->val);
+}
 
 
 
--- linked_stack.h
+++ linked_stack.h
@@ -5,12 +5,12 @@
 #ifndef C__PRIME_LINKED_STACK_H
 #define C__PRIME_LINKED_STACK_H
 
-
+template<typename T>
 class LinkedStack {
     struct Node {
-        int val;
+        T val;
         Node *next{nullptr};
-        explicit Node(int val) : val(val) {}
+        explicit Node(T val) : val(val) {}
     };
     int size;
     Node *top;
@@ -18,12 +18,13 @@ class LinkedStack {
 
     //构造 析构
     LinkedStack();
+
     ~LinkedStack();
 
     //
-    void push(int value);
-    int pop();
-    int peek() const;
+    void push(T value);
+    T pop();
+    T peek() const;
 
     //
     bool is_empty() const;
@@ -32,5 +33,84 @@ class LinkedStack {
 };
 
 
+#include <print>
+#include <stdexcept>
+
+template<typename T>
+LinkedStack<T>::LinkedStack() {
+    size=0;
+    top=nullptr;
+}
+
+template<typename T>
+LinkedStack<T>::~LinkedStack() {
+    size=0;
+    auto curr=top;
+    while (top!=nullptr) {
+        top=top->next;
+        delete curr;
+        curr=top;
+    }
+}
+
+template<typename T>
+void LinkedStack<T>::push(T value) {
+    auto new_node=new Node(value);
+    if (size==0) {
+        top=new_node;
+    }else {
+        new_node->next=top;
+        top=new_node;
+    }
+    size++;
+}
+
+template<typename T>
+T LinkedStack<T>::pop() {
+    if (size==0) {
+        throw std::out_of_range("stack is empty");
+    }
+    auto curr=top;
+    top=top->next;
+    T temp=curr->val;
+    delete curr;
+    size--;
+    if (size==0) {
+        top=nullptr;
+    }
+    return temp;
+}
+
+template<typename T>
+T LinkedStack<T>::peek() const {
+    if (size==0) {
+        throw std::out_of_range("stack is empty");
+    }
+    return top->val;
+}
+
+template<typename T>
+bool LinkedStack<T>::is_empty() const{
+    return size==0;
+}
+
+template<typename T>
+void LinkedStack<T>::display() const {
+    if (size==0) {
+        std::println("empty stack");
+        return;
+    }
+    auto curr=top;
+    while (curr->next!=nullptr) {
+        std::print("{}->", curr->val);
+        curr=curr->next;
+    }
+    std::println("{}", curr->val);
+}
+
+template <typename T>
+int LinkedStack<T>::get_size() const {
+    return size;
+}
 
 #endif //C__PRIME_LINKED_STACK_H
